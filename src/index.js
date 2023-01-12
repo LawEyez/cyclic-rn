3f9f6dffd450755da20f49e6cafabb7c842978a8
Bioga Allen <allenojuka10@gmail.com>
2023-01-12 11:23:16 +0300

    Setup.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import 'module-alias/register.js'
+import 'dotenv/config'
 
-dotenv.config()
+import express from 'express'
+
+/** Resource imports */
+import post from './resources/post/index.js'
 
 const app = express()
 const port = process.env.PORT || 5000
 
 // Middleware.
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
+// Routes.
 app.get('/', (req, res) => {
   res.send('Hello Universe')
 })
+app.use('/api/posts', post)
 
 // Start server.
 app.listen(port, () =>
