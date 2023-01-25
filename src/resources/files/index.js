b65b038cf75c4c3449884048f420d9107271a016
Bioga Allen <allenojuka10@gmail.com>
2023-01-25 09:25:33 +0300

    Remove unused imports.

--- src/resources/files/index.js
+++ src/resources/files/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { createPresignedPost } from '@aws-sdk/s3-presigned-post'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { S3Client, ListObjectV2Command, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client } from '@aws-sdk/client-s3'
 
 // Init router.
 const router = Router()
